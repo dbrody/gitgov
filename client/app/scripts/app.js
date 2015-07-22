3b186c43c9c4ef639d438bef4de630b7839f26c9
David Brody <dbrody@ideo.com>
2015-07-22 14:00:01 -0700

    Updates some styles and removes some files.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -18,7 +18,6 @@ var app = angular.module('demoApp', [
     'ng-token-auth',
     'restangular',
     'ui.router',
-    'textAngular',
     'ui.utils',
     'ui.bootstrap',
     'angulartics',
@@ -30,112 +29,20 @@ app.config(function ($provide, $stateProvider, $urlRouterProvider, $authProvider
   RestangularProvider.setBaseUrl('/api/v1');
 
   // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise('/moments/all');
+  $urlRouterProvider.otherwise('/');
 
   // Now set up the states
   $stateProvider
     .state('root', {
-      templateUrl: 'views/index.html',
-      controller: 'MainCtrl'
-    })
-    .state('root.home', {
-      url: '/?reset_password_token&confirmed',
+      url: '/',
       templateUrl: 'views/main.html',
       controller: 'MainCtrl'
-    })
-    .state('root.profile', {
-      url: '/profile',
-      templateUrl: 'views/profile.html',
-      controller: 'ProfileCtrl'
-    })
-
-    // Moments View
-    .state('root.moments', {
-      templateUrl: 'views/moments/index.html',
-      controller: 'MomentsCtrl',
-      resolve: {
-        auth: ['$auth', function($auth) {
-          return $auth.validateUser();
-        }]
-      }
-    })
-    .state('root.moments.all', {
-      url: '/moments/all',
-      templateUrl: 'views/moments/all.html'
-    })
-    .state('root.moments.moments', {
-      url: '/moments/moments',
-      templateUrl: 'views/moments/by_moment.html'
-    })
-
-    // States for prompts
-    .state('root.prompt', {
-      templateUrl: 'views/prompts/main.html',
-      controller: 'PromptCtrl',
-      resolve: {
-        auth: ['$auth', function($auth) {
-          return $auth.validateUser();
-        }]
-      }
-    })
-    .state('root.prompt.inspiration', {
-      url: '/prompt/inspiration',
-      templateUrl: 'views/prompts/inspiration.html'
-    })
-    .state('root.prompt.experience', {
-      url: '/prompt/experience',
-      templateUrl: 'views/prompts/experience.html'
-    })
-    .state('root.prompt.accomplishment', {
-      url: '/prompt/accomplishment',
-      templateUrl: 'views/prompts/accomplishment.html'
-    })
-
-    // States for registration
-    .state('root.register', {
-      templateUrl: 'views/user_registrations/new.html',
-      controller: 'UserRegistrationsCtrl'
-    })
-    .state('root.register.start', {
-      url: '/register',
-      templateUrl: 'views/user_registrations/basics.html',
-      controller: 'UserRegistrationsCtrl'
-    })
-
-    // Sign In State
-    .state('root.sign_in', {
-      url: '/sign_in',
-      templateUrl: 'views/user_sessions/new.html',
-      controller: 'UserSessionsCtrl'
     });
 
   // Configure authentication endpoints / prefix
   $authProvider.configure({
       apiUrl: '/api/v1'
   });
-
-
-  // Used for customizing wysiwyg toolbar options for moment content fields
-  $provide.decorator('taOptions', ['taRegisterTool', '$delegate',
-    function(taRegisterTool, taOptions){
-    // Customize textAngular's toolbar
-
-    /*
-    taOptions.toolbar = [
-        ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
-        ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
-        ['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
-        ['html', 'insertImage','insertLink', 'insertVideo', 'wordcount', 'charcount']
-    ];
-    */
-
-    taOptions.toolbar = [
-      ['html', 'insertImage','insertLink', 'insertVideo'],
-      //['h1', 'h3', 'h6', 'quote'],
-      ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol']
-    ];
-    return taOptions;
-  }]);
 });
 
 
